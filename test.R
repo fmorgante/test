0c7835c19e78b8b0209e8a10301467f9f64931d0
fmorgante <fmorgante@uchicago.edu>
2018-08-01 11:31:36 -0500

    Increase the number of samples

--- test.R
+++ test.R
@@ -8,8 +8,14 @@
 ###Drawn 100 samples from a normal distribution with mean=0 and sd=2
 samples <- rnorm(100, mean=0, sd=2)
 
-###Check mean and sd)
+###Check mean and sd
 mean(samples)
-sd(samples) 
+sd(samples)
 
+###Let's add more samples 
+samples1000 <- rnorm(1000, mean=0, sd=2)
+
+###Check mean and sd 
+mean(samples1000)
+sd(samples1000)
 

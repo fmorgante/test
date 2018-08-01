11a4eda5b1079fce9e52289bcfedbe5c5a3c4d68
fmorgante <fmorgante@uchicago.edu>
2018-08-01 15:06:14 -0500

    Add first version of the file

---
+++ test_cluster.R
@@ -0,0 +1,13 @@
+#############################
+## Fabio Morgante
+## 08/01/2018
+##
+## R script to practice with git and github on the cluster
+#############################
+
+###Drawn 100 samples from a normal distribution with mean=0 and sd=2
+samples <- rnorm(100, mean=0, sd=2)
+
+###Check mean and sd
+mean(samples)
+sd(samples)

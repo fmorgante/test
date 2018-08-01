25cd2f270b1ecdbc6bfb9fc938f0833ca0af6ed8
fmorgante <fmorgante@uchicago.edu>
2018-08-01 11:27:29 -0500

    Add file

---
+++ test.R
@@ -0,0 +1,15 @@
+#############################
+## Fabio Morgante
+## 08/01/2018
+##
+## R script to practice with git and github
+#############################
+
+###Drawn 100 samples from a normal distribution with mean=0 and sd=2
+samples <- rnorm(100, mean=0, sd=2)
+
+###Check mean and sd)
+mean(samples)
+sd(samples) 
+
+

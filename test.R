cdbebc0b21b7e0204f4071ced034fbc2100a852a
fmorgante <fmorgante@uchicago.edu>
2018-08-01 11:53:46 -0500

    Calculate variance as well

--- test.R
+++ test.R
@@ -8,22 +8,25 @@
 ###Drawn 100 samples from a normal distribution with mean=0 and sd=2
 samples <- rnorm(100, mean=0, sd=2)
 
-###Check mean and sd
+###Check mean, sd and variance
 mean(samples)
 sd(samples)
+var(samples)
 
 ###Let's add more samples 
 samples1000 <- rnorm(1000, mean=0, sd=2)
 
-###Check mean and sd 
+###Check mean, sd and variance
 mean(samples1000)
 sd(samples1000)
+var(samples1000)
 
 ###Let's add even more samples
 samples10000 <- rnorm(10000, mean=0, sd=2)
 
-###Check mean and sd
+###Check mean, sd and variance
 mean(samples10000)
 sd(samples10000)
+var(samples10000)
 
 

89896927375c22016f016c7cfa7a9387861dd19c
fmorgan3 <fabiomorganz@gmail.com>
2020-08-25 13:26:00 -0400

    Revert last commit

--- test.R
+++ test.R
@@ -5,7 +5,7 @@
 ## R script to practice with git and github
 #############################
 
-#### Drawn 100 samples from a normal distribution with mean=0 and sd=2
+### Drawn 100 samples from a normal distribution with mean=0 and sd=2
 samples <- rnorm(100, mean=0, sd=2)
 
 ###Check mean, sd and variance
